2a6b4ef51981b60ff89760b71544c4f8ac987a5d
Sarmad Ahmed Khalil <41991331+sarmadkhalil@users.noreply.github.com>
2023-11-30 19:55:44 +0800

    initializing library to get data from tmdb

---
+++ packages/sarmad/tmdb-client/src/TmdbClientServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sarmad\TmdbClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class TmdbClientServiceProvider extends ServiceProvider
+{
+    public function boot() 
+    {
+
+    }
+
+    public function run()
+    {
+        
+    }
+}
